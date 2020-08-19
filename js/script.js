6e93884b133d8a08c030224934f4bbce5d24ed96
Eko Prasetio <mre_pras@programmer.net>
2020-08-19 16:20:51 +0700

    Update Contact Info

--- js/script.js
+++ js/script.js
@@ -32,3 +32,20 @@ $('.jumbotron .text-paralax').each(function(i) {
   }, 180 * (i+1))
 
 });
+
+//ketika di scroll muncul icon scroll ke atas
+  $(window).scroll(function () {
+        var totalHeight = $(window).scrollTop();
+        if (totalHeight > 400) {
+            $(".scrolltotop").fadeIn();
+        } else {
+            $(".scrolltotop").fadeOut();
+        }
+    });
+
+ //proses scroll
+    $('a.scrolltotop').on('click', function (event) {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#home").offset().top
+          }, 800);
+    });
